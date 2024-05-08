5839c8dfe08d7292e7443207fc6839a7264380dc
Thilliê Cristina Moreira de Souza <thillie.moreira@gmail.com>
2024-05-08 08:54:33 -0300

    Implementação da função Controller no Projeto Conta Bancária

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -14,13 +14,13 @@ public class Menu {
 	
 	public static Scanner leia = new Scanner(System.in);
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		ContaController contas = new ContaController();
 		
-		int opcao, numero, agencia, tipo, aniversario;
+		int opcao, numero, agencia, tipo, aniversario, numeroDestino;
 		String titular;
-		float saldo, limite;
+		float saldo, limite, valor;
 		
 		System.out.println("\nCriar Contas\n");
 		
@@ -136,6 +136,11 @@ public class Menu {
 			case 3: {
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da Conta - por número\n\n");
 				
+				System.out.println("Digite o número da conta: ");
+				numero = leia.nextInt();
+				
+				contas.procurarPorNumero(numero);
+				
 				keyPress();
 				break;
 			}
@@ -143,12 +148,61 @@ public class Menu {
 			case 4: {
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da Conta\n\n");
 				
+				System.out.println("Digite o número da conta: ");
+				numero = leia.nextInt();
+				
+				var buscaConta = contas.buscarNaCollection(numero);
+				
+				if(buscaConta != null) {
+					
+					tipo = buscaConta.getTipo();
+					
+					System.out.println("Digite o Número da Agência: ");
+					agencia = leia.nextInt();
+					System.out.println("Digite o Nome do Titular: ");
+					leia.skip("\\R?");
+					titular = leia.nextLine();
+					
+					System.out.println("Digite o Saldo da Conta (R$): ");
+					saldo = leia.nextFloat();
+					
+					switch (tipo ) {
+						case 1 -> {
+							System.out.println("Digite o Limite de Crédito (R$): ");
+							limite = leia.nextFloat();
+							
+							contas.atualizar(new ContaCorrente(numero, agencia, tipo, titular, saldo, limite));
+							
+					}
+						case 2 -> {
+							System.out.println("Digite o dia do Aniversário da Conta: ");
+							aniversario = leia.nextInt();
+							
+							contas.atualizar(new ContaPoupanca(numero, agencia, tipo, titular, saldo, aniversario));
+						
+						}
+						default -> {
+							System.out.println("Tipo de conta inválido!");
+						}
+					
+					}
+				}else {
+						System.out.println("A Conta não foi encontrada!");
+					}
+						
+				}
+				
 				keyPress();
 				break;
-			}
+				
 			
 			case 5: {
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a Conta\n\n");
+				
+				System.out.println("Digite o número da Conta: ");
+				numero = leia.nextInt();
+				
+				contas.deletar(numero);
 				
 				keyPress();
 				break;
@@ -157,6 +211,16 @@ public class Menu {
 			case 6: {
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
 				
+				System.out.println("Digite o Número da Conta: ");
+				numero = leia.nextInt();
+				
+				do {
+					System.out.println("Digite o Valor do Saque (R$): ");
+					valor = leia.nextFloat();
+				}while(valor <= 0);
+				
+				contas.sacar(numero, valor);
+				
 				keyPress();
 				break;
 			}
@@ -164,12 +228,34 @@ public class Menu {
 			case 7: {
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
 				
+				System.out.println("Digite o Número da Conta: ");
+				numero = leia.nextInt();
+						
+				do {
+					System.out.println("Digite o Valor do Depósito (R$): ");
+					valor = leia.nextFloat();
+				}while(valor <= 0);
+				
+				contas.depositar(numero, valor);
+				
 				keyPress();
 				break;
 			}
 			
 			case 8: {
 				System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre Contas\n\n");
+				
+				System.out.println("Digite o Número da Conta de Origem: ");
+				numero = leia.nextInt();
+				System.out.println("Digite o Número da Conta de Destino: ");
+				numeroDestino = leia.nextInt();
+				
+				do {
+					System.out.println("Digite o Valor da Transferência (R$): ");
+					valor = leia.nextFloat();
+				}while(valor <= 0);
+				
+				contas.transferir(numero, numeroDestino, valor);
 				
 				keyPress();
 				break;
